91d80b53e5efd0b4bd18ad18fafdbd8973636dca
Katlego Kgotse <katlegokgotse88@gmail.com>
2025-04-03 08:56:31 +0200

    Main: Fixed the navigation bar and responsiveness

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,6 +1,26 @@
-function openNavigationBar(){
-    document.getElementById("navbar").style.width = "250px";
-}
-function closeNavigationBar(){
-    document.getElementById("navbar").style.height = "0%"
-}
+var tl = gsap.timeline();
+var main = document.querySelector("#nav i");
+var cursor = document.querySelector("#full i");
+
+tl.to("#full", {
+    right: 0,
+    duration: 0.4
+});
+tl.from("#full li a", {
+    x: 150,
+    opacity: 0,
+    duration: 0.5,
+    stagger: 0.3
+});
+tl.from("#full i", {
+    opacity: 0,
+    x: 150,
+    duration: 0.5
+});
+tl.pause();
+main.addEventListener("click", () => {
+    tl.play();
+});
+cursor.addEventListener("click", () => {
+    tl.reverse();
+});
