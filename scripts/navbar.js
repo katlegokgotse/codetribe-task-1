5cba5833ff73a4251f87ccf49246ad8709ef8b59
Katlego Kgotse <katlegokgotse88@gmail.com>
2025-04-03 03:05:06 +0200

    Responsive

---
+++ scripts/navbar.js
@@ -0,0 +1,6 @@
+function openNavigationBar(){
+    document.getElementById("navbar").style.width = "250px";
+}
+function closeNavigationBar(){
+    document.getElementById("navbar").style.height = "0%"
+}
